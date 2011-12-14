ecfbe06a86f2ec60bddd827421800dfc48be7b3d
Thorbjørn Lindeijer <thorbjorn@lindeijer.nl>
2011-12-14 20:01:29 +0100

    Added a node.js/express based server
    It makes the files available through HTTP on port 3000, for easy access
    by a browser.

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var app = express.createServer();
+
+app.configure(function(){
+  app.get('/version', function(req, res) {
+      res.send('0.0.1');
+  });
+  app.use(express.logger());
+  app.use(express.static(__dirname));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+app.listen(3000);
+
+console.log('TMW.js server listening on port %s', app.address().port);
